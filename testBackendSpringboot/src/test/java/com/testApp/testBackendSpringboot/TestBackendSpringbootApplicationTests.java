a10e2f50b6adab7f7ad5fd6f27b0245d379ba76d
Alison Zerbe <alisonkzerbe@gmail.com>
2020-12-31 00:31:06 -0800

    adds springboot setup

---
+++ testBackendSpringboot/src/test/java/com/testApp/testBackendSpringboot/TestBackendSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testApp.testBackendSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBackendSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
